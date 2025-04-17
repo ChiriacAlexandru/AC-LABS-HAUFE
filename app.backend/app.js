29781968184653c601a7aaf407c7b717c0982884
Alexandru Chiriac <chiriac1910@gmail.com>
2025-04-18 00:10:06 +0300

    conectare la baza de date mongodb atals cu succes

--- app.backend/app.js
+++ app.backend/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose');
+
+
 require("dotenv").config();
 
+const mongoURI = process.env.MONGODB_URI;
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -15,3 +19,9 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+// Connect to MongoDB Atlas
+mongoose.connect(mongoURI)
+    .then(() => console.log('Conectat la MongoDB Atlas!'))
+    .catch(err => console.error('Eroare la conectare:', err));
